72b9ccbc369c034c80d307b3dca80feba7d8a5d6
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-09-07 11:48:13 +0200

    Add NoteCreation component with CSS

--- components/App.jsx
+++ components/App.jsx
@@ -1,8 +1,10 @@
 import Note from "./Note";
+import NoteCreation from "./NoteCreation";
 
 function App() {
   return (
     <div>
+      <NoteCreation />
       <Note />
       <Note />
       <Note />
