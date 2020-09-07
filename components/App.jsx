858459b5a8e7ce652d76fb892c45640d4e42b8c9
Alfonso Cartes Guilarte <alfonsocartesg@gmail.com>
2020-09-07 11:39:53 +0200

    Responsive test with multiple notes

--- components/App.jsx
+++ components/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <Note />
+      <Note />
+      <Note />
     </div>
   );
 }
--- components/Note.jsx
+++ components/Note.jsx
@@ -2,6 +2,7 @@ function Note() {
   return (
     <div className="note">
       <h1>This is the note title</h1>
+      <hr />
       <p>This is the note content</p>
     </div>
   );
